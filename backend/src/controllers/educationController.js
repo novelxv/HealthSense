47fecb4b7af589839ae5dacb92d2a31de0903b00
Novelya Putri Ramadhani <114747134+novelxv@users.noreply.github.com>
2025-01-31 14:27:54 +0700

    feat(be): add education to database

--- backend/src/controllers/educationController.js
+++ backend/src/controllers/educationController.js
@@ -2,7 +2,17 @@ const EducationModel = require("../models/educationModel");
 
 const getEducationArticles = async (req, res) => {
   try {
-    const articles = await EducationModel.getAllEducationArticles();
+    const { category, location } = req.query;
+
+    let articles;
+    if (category) {
+      articles = await EducationModel.getEducationByCategory(category);
+    } else if (location) {
+      articles = await EducationModel.getEducationByLocation(location);
+    } else {
+      articles = await EducationModel.getAllEducationArticles();
+    }
+
     res.json(articles);
   } catch (error) {
     res.status(500).json({ message: "Error fetching education articles", error });
--- backend/src/models/educationModel.js
+++ backend/src/models/educationModel.js
@@ -1,7 +1,18 @@
 const knex = require("../database/knex");
 
 const getAllEducationArticles = async () => {
-  return await knex("education").select("*");
+  return await knex("education").select("*").orderBy("created_at", "desc");
 };
 
-module.exports = { getAllEducationArticles };
+const getEducationByCategory = async (category) => {
+  return await knex("education").where("category", category).orderBy("created_at", "desc");
+};
+
+const getEducationByLocation = async (location) => {
+  return await knex("education")
+    .where("location", location)
+    .orWhereNull("location")
+    .orderBy("created_at", "desc");
+};
+
+module.exports = { getAllEducationArticles, getEducationByCategory, getEducationByLocation };
